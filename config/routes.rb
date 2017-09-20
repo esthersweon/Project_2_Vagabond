74dff0ff13b7910d919c847b8ec399f5d0ba1f8b
Huan Ming Liao <hmliao14@gmail.com>
2017-09-20 15:52:38 -0700

    initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "/"
+  # root to: ""
 
   # new display form html page for creating new profile
   get "/users/new", to: "users#new", as: "new_user"
   # creating and insert into db
   post "/users", to: "users#create"
   # get -> showing individual profile
-  get "/users/:id", to: "users#show", as: "user"
+  get "/users/:user_id", to: "users#show", as: "user"
 
   #Sessions
-  get '/login', to: 'sessions#new'
-  get '/logout', to: 'sessions#destroy'
-  post '/sessions', to: 'sessions#create'
+  # get '/login', to: 'session#new'
+  # get '/logout', to: 'session#destroy'
+  # post '/session', to: 'session#create'
 
-  #Libraries
-  get '/libraries', to: 'libraries#index'
-  get '/libraries/new', to: 'libraries#new', as: 'new_library'
-  post '/libraries', to: 'libraries#create'
-  get "/libraries/:id", to: "libraries#show", as: 'library'
+  # Posts routes
+
+
+
+  # index -> Homepage showing list of cities to select
+  # get '/cites', to: 'cities#index'
+  # # show specific city
+  # get '/cities/:id/show', to: 'cities#show', as: "city"
+  # get "/libraries/:id", to: "libraries#show", as: 'library'
 
 end
